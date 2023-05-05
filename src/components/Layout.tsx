4e3d915e93fdfc0d2a9c97998720436303563626
Simlex <gettosimlex@gmail.com>
2023-05-05 10:24:15 +0100

    Added toast card UI

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,10 @@
-import { FunctionComponent, ReactElement } from "react";
+import { FunctionComponent, ReactElement, useContext } from "react";
 import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { ToastContext } from "@/extensions/toast";
+import { ToastMessageType } from "./models/ToastMessageType";
+import ToastCard from "./Card/ToastCard";
 // import Inter from 'next/font';
 
 interface LayoutProps {
@@ -9,8 +12,18 @@ interface LayoutProps {
 }
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }): ReactElement => {
+
+    const toastContext = useContext(ToastContext);
+
     return (
         <>
+        <ToastCard
+            visibility={toastContext?.toastOptions?.visible ?? false}
+            title={toastContext?.toastOptions?.title ?? 'Welcome'}
+            description={toastContext?.toastOptions?.description ?? ''}
+            messageType={toastContext?.toastOptions?.type ?? ToastMessageType.Info}
+            timeout={toastContext?.toastOptions?.timeout ?? 0.01} />
+
             <Head>
                 <link
                     rel="stylesheet"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,100 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { IToastOptions } from '../components/models/toastOptions';
+import { ToastMessageType } from '../components/models/ToastMessageType';
+import { ToastContext } from '../extensions/toast';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+
+  const [toastOptions, setToastOptions] = useState<IToastOptions>({
+    type: ToastMessageType.Info,
+    title: 'Welcome',
+    description: '',
+    timeout: 0.01,
+    visible: false
+  });
+
+  /**
+   * Logs success message
+   * @param title The title of the success
+   * @param description The description of the success
+   * @param timeout The display timeout of the toast
+   */
+  function logSuccess(title: string, description: string, timeout: number = 4000) {
+    setToastOptions({
+      type: ToastMessageType.Success,
+      title: title,
+      description: description,
+      timeout: timeout,
+      visible: true
+    });
+  }
+
+  /**
+   * Logs info message
+   * @param title The title of the info
+   * @param description The description of the info
+   * @param timeout The display timeout of the toast
+   */
+  function logInfo(title: string, description: string, timeout: number = 4000) {
+    setToastOptions({
+      type: ToastMessageType.Info,
+      title: title,
+      description: description,
+      timeout: timeout,
+      visible: true
+    });
+  }
+
+  /**
+   * Logs warning message
+   * @param title The title of the warning
+   * @param description The description of the warning
+   * @param timeout The display timeout of the toast
+   */
+  function logWarning(title: string, description: string, timeout: number = 4000) {
+    setToastOptions({
+      type: ToastMessageType.Warning,
+      title: title,
+      description: description,
+      timeout: timeout,
+      visible: true
+    });
+  }
+
+  /**
+   * Logs error message
+   * @param title The title of the error
+   * @param description The description of the error
+   * @param timeout The display timeout of the toast
+   */
+  function logError(title: string, description: string, timeout: number = 4000) {
+    setToastOptions({
+      type: ToastMessageType.Error,
+      title: title,
+      description: description,
+      timeout: timeout,
+      visible: true
+    });
+  }
+
+  function closeToast() {
+    setToastOptions({
+      type: ToastMessageType.None,
+      title: '',
+      description: '',
+      timeout: 0.01,
+      visible: false
+    });
+  }
+
+  return <>
+    <ToastContext.Provider value={{ toastOptions, logSuccess, logInfo, logWarning, logError, closeToast }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ToastContext.Provider>
+  </>
 }
