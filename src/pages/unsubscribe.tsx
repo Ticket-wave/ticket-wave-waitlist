abf0909df1c77b489d8b1d92403a453aae070b0a
Simlex <gettosimlex@gmail.com>
2023-05-23 08:34:02 +0100

    fixed errors

--- src/pages/unsubscribe.tsx
+++ src/pages/unsubscribe.tsx
@@ -21,11 +21,11 @@ const Unsubscribe: FunctionComponent<UnsubscribeProps> = (): ReactElement => {
                 initial={{ opacity: 0, scale: 1, y: 80 }}
                 whileInView={{ opacity: 1, scale: 1, y: 0 }}
                 transition={{ duration: 0.6, ease: 'easeIn' }}>
-                <p>Oh no! We're sad to see you go! You've successfully unsubscribed from our ticket sales website. We'll miss sharing exciting events, hot deals, and backstage secrets with you.</p>
+                <p>Oh no! We&apos;re sad to see you go! You&apos;ve successfully unsubscribed from our ticket sales website. We&apos;ll miss sharing exciting events, hot deals, and backstage secrets with you.</p>
 
-                <p>But hey, if you ever change your mind and decide you can't resist the allure of tickets and epic experiences, we'll be here, ready to welcome you back with open arms. Just come on over to our website and resubscribe to join the party again!</p>
+                <p>But hey, if you ever change your mind and decide you can&apos;t resist the allure of tickets and epic experiences, we&apos;ll be here, ready to welcome you back with open arms. Just come on over to our website and resubscribe to join the party again!</p>
 
-                <p>We'd love to hear why you unsubscribed. Was it something we said? Or did a magical unicorn steal your interest? Feel free to share your thoughts with us. We're always looking to improve and make your ticket-buying journey more extraordinary.</p>
+                <p>We&apos;d love to hear why you unsubscribed. Was it something we said? Or did a magical unicorn steal your interest? Feel free to share your thoughts with us. We&apos;re always looking to improve and make your ticket-buying journey more extraordinary.</p>
 
                 <p>Thank you for being part of our ticket-loving community. Remember, the stage is always set for your triumphant return!</p>
             </motion.div>
